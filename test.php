1afe2d16b4f562125612d6e8edcaa217349b44cf
Suraj Kumar Jana <surajjana2@gmail.com>
2016-04-23 12:15:30 +0530

    new commit

--- test.php
+++ test.php
@@ -1,22 +1,29 @@
 <?php  
 	require_once("conf/constants.php");
 
-	$conn = mysql_connect(HOST,USER,PASSWORD);
-
-	if(! $conn )
-	{
-	  die('Could not connect: ' . mysql_error());
+	$activity_code = '16/SA/00001';
+	$arr = explode("/",$activity_code);
+	$val = (int)$arr[2];
+	$val += 1;
+	$mnth = date("m");
+	if((int)$mnth == 4){
+		$res = (string)((int)date("y") + 1).'/SA/';
+	}else{
+		$res = (string)date("y").'/SA/';
 	}
-
-	mysql_select_db(DB);
-
-	$sql = "select email from user where rights=3";
-
-	$retval = mysql_query( $sql, $conn );
-
-	if(! $retval )
-	{
-	  die('Could not get data: ' . mysql_error());
+	if($val<10){
+		$res .= '0000'.(string)$val;
+	}elseif($val>9 && $val<100){
+		$res .= '000'.(string)$val;
+	}elseif($val>99 && $val<1000){
+		$res .= '00'.(string)$val;
+	}elseif($val>999 && $val<10000){
+		$res .= '0'.(string)$val;
+	}else{
+		$res .= (string)$val;
 	}
+	$activity_id = $res;
+
+	echo $activity_id;
 
 ?>
